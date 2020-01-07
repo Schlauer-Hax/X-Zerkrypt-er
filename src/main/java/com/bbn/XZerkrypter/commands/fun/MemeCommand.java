29d1759385a007d9bd6b0ed0b2a4f6c951c9c502
GregTCLTK <gregtcltk@gmail.com>
2020-01-07 19:40:24 +0100

    Another BotPlus command

--- src/main/java/com/bbn/XZerkrypter/commands/fun/MemeCommand.java
+++ src/main/java/com/bbn/XZerkrypter/commands/fun/MemeCommand.java
@@ -4,6 +4,7 @@ package com.bbn.XZerkrypter.commands.fun;
  * @Author Skidder / GregTCLTK
  */
 
+import com.bbn.XZerkrypter.XZerkrypter;
 import com.bbn.XZerkrypter.commands.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -21,24 +22,33 @@ public class MemeCommand implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url("https://some-random-api.ml/meme").build();
+        if (XZerkrypter.BotPlus.contains(event.getAuthor().getId())) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url("https://some-random-api.ml/meme").build();
 
-        try {
-            Response response = client.newCall(request).execute();
-            JSONObject json = new JSONObject(response.body().string());
-            String url = json.get("image").toString();
+            try {
+                Response response = client.newCall(request).execute();
+                JSONObject json = new JSONObject(response.body().string());
+                String url = json.get("image").toString();
+                event.getTextChannel().sendMessage(new EmbedBuilder()
+                        .setTitle("Dein Meme")
+                        .setImage(url)
+                        .setColor(Color.GREEN)
+                        .setTimestamp(Instant.now())
+                        .build()).queue();
+            } catch (IOException e) {
+                e.printStackTrace();
+                event.getTextChannel().sendMessage(new EmbedBuilder()
+                        .setTitle("Fehler!")
+                        .setDescription("Die Anfrage an die Meme API ist fehlgeschlagen. Bitte kontaktiere Skidder#6775 auf Discord.")
+                        .setColor(Color.RED)
+                        .setTimestamp(Instant.now())
+                        .build()).queue();
+            }
+        } else {
             event.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setTitle("Dein Meme")
-                    .setImage(url)
-                    .setColor(Color.GREEN)
-                    .setTimestamp(Instant.now())
-                    .build()).queue();
-        } catch (IOException e) {
-            e.printStackTrace();
-            event.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setTitle("Fehler!")
-                    .setDescription("Die Anfrage an die Meme API ist fehlgeschlagen. Bitte kontaktiere Skidder#6775 auf Discord.")
+                    .setTitle("Kein Bot Plus")
+                    .setDescription("Du benötigst den Bot Plus Status um diesen Command ausführen zu können.")
                     .setColor(Color.RED)
                     .setTimestamp(Instant.now())
                     .build()).queue();
